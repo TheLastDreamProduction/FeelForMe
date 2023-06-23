32c0f02de0364ebfcc500caebac7c5d5be364701
TheLastDreamStudio <132101840+TheLastDreamStudio@users.noreply.github.com>
2023-06-23 13:57:02 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector('#emoji')
+
+const emojis = [
+    "😊",
+    "🤣",
+    "😒",
+    "🍆",
+    "😗",
+    "😃",
+    "😟",
+];
+
+btn.addEventListener('mouseover', () =>{
+    btn.innerHTML = emojis[Math.floor(Math.random() * emojis.length)];
+})
